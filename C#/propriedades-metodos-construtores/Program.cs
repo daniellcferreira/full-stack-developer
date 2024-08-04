1bcc2168bc76fa18de0bb7de772b6aa5ba37c988
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-08-04 16:35:29 -0300

    create new lesson

---
+++ C#/propriedades-metodos-construtores/Program.cs
@@ -0,0 +1,12 @@
+﻿using propriedades_metodos_construtores.Models;
+
+Pessoa p1 = new Pessoa("Pedro", "Henrique");
+Pessoa p2 = new Pessoa("Eduardo", "Novais");
+
+Curso cursoProgramacao = new Curso();
+cursoProgramacao.Nome = "Programação";
+cursoProgramacao.Alunos = new List<Pessoa>();
+
+cursoProgramacao.AdicionarAluno(p1);
+cursoProgramacao.AdicionarAluno(p2);
+cursoProgramacao.ListaAlunos();

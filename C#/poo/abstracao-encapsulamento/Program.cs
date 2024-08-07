402b847242c357b9e75f79161616e3a59246063b
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-08-07 12:10:37 -0300

    create new lesson

---
+++ C#/poo/abstracao-encapsulamento/Program.cs
@@ -0,0 +1,13 @@
+﻿using abstracao_encapsulamento.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Pedro";
+p1.Idade = 30;
+
+p1.Apresentar();
---
+++ C#/poo/heranca-polimorfismo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using heranca_polimorfismo.Models;
+
+Aluno a1 = new Aluno();
+a1.Nome = "Pedro";
+a1.Idade = 25;
+a1.Nota = 7.5;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Eduardo";
+p1.Idade = 45;
+p1.Salario = 2000;
+p1.Apresentar();

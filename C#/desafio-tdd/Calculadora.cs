098e8b621f68cb826605c6517e76b3f66e9371ba
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-08-14 10:40:04 -0300

    create new lesson and project

---
+++ C#/desafio-tdd/Calculadora.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewTalents
+{
+    public class Calculadora
+    {
+        private List<string> listaHistorico;
+
+        // usar a data no historico
+        private string data;
+
+        //construtor
+        public Calculadora(string data)
+        {
+            listaHistorico = new List<string>();
+            this.data = data;
+        }
+
+        public int somar(int a, int b)
+        {
+            int resultado = a + b;
+            listaHistorico.Insert(0, $"resultado {resultado} - data:  {data}");
+            return resultado;
+        }
+
+        public int Subtrair(int a, int b)
+        {
+            int resultado = a - b;
+            listaHistorico.Insert(0, $"resultado {resultado} - data:  {data}");
+            return resultado;
+        }
+
+        public int multiplicar(int a, int b)
+        {
+            int resultado = a * b;
+            listaHistorico.Insert(0, $"resultado {resultado} - data:  {data}");
+            return resultado;
+        }
+
+        public int dividir(int a, int b)
+        {
+            int resultado = a / b;
+            listaHistorico.Insert(0, $"resultado {resultado} - data:  {data}");
+            return resultado;
+        }
+
+        public List<string> historico()
+        {
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
+        }
+
+    }
+}

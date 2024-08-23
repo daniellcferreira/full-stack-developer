05cae68c91306755dc5c1895d5e08478eab878cb
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-08-23 14:30:36 -0300

    create new project

---
+++ React/desafio-screen/src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title, onClick }: IButtonProps) => {
+  return (
+    <ButtonContainer onClick={onClick} title={title}>
+      {title}
+    </ButtonContainer>
+  );
+};
+
+export default Button;
---
+++ React/desafio-screen/src/pages/Login/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input } from "../../components";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SignUpValidationSchema } from "../../validation/schemas";
+
+import { Container, LoginContainer, Column, Spacing, Title } from "./styles";
+import { IFormLogin, defaultValues } from "./types";
+
+const Login = () => {
+  const {
+    control,
+    formState: { errors, isValid },
+  } = useForm<IFormLogin>({
+    resolver: yupResolver(SignUpValidationSchema),
+    mode: "onBlur",
+    defaultValues,
+    reValidateMode: "onChange",
+  });
+
+  return (
+    <Container>
+      <LoginContainer>
+        <Column>
+          <Title>Login</Title>
+          <Spacing />
+          <Input
+            name="email"
+            placeholder="Email"
+            control={control}
+            errorMessage={errors?.email?.message}
+          />
+          <Spacing />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            control={control}
+            errorMessage={errors?.password?.message}
+          />
+          <Spacing />
+          <Button title="Entrar" />
+        </Column>
+      </LoginContainer>
+    </Container>
+  );
+};
+
+export default Login;
